9c95fbb55ebce31ef0571d143f9e01992594de6d
Danielle Smith <57499759+Danielle-Smith@users.noreply.github.com>
2019-11-07 18:38:43 -0700

    Add files via upload

---
+++ prizewinners.py
@@ -0,0 +1,10 @@
+import random
+
+contestants = ['Doug', 'Frank', 'Sam', 'Kadence', 'Oliver', 'Lisa', 'Gibson', 'Nate']
+first_place = random.choice(contestants)
+
+second_place = random.choice(contestants)
+
+third_place = random.choice(contestants)
+
+print('1st place:', first_place,'  ' '2nd place:', second_place,'  ' '3rd place:', third_place)
